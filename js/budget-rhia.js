035f9f7c3a6e29856c372d9913a453377a68bc25
risaco <rhia.cooper@gmail.com>
2017-08-19 10:12:55 -0500

    moved my code to -rhia files and re-copied original master files

--- js/budget-rhia.js
+++ js/budget-rhia.js
@@ -2,31 +2,33 @@ $(document).ready(function() {
   //Input zip code-Page 1
 
   //Submit zip
-  $('#zip-submit').on('click', function(event) {
-    event.preventDefault();
-    //grab zip
-    var zipcode = $('#zip-input').val().trim();
+  // $('#zip-submit').on('click', function(event) {
+  //   event.preventDefault();
+  //   //grab zip
+  //   var zipcode = $('#zip-input').val().trim();
 
-    //call API to find state of zip-input
-    var key = "AIzaSyAaK5LW94pb1MJ1T8qZ_am1xvXEdwEuC0w";
-    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode + "&key=" + key;;
-    console.log(queryURL);
+  //   //call API to find state of zip-input
+  //   var key = "AIzaSyAaK5LW94pb1MJ1T8qZ_am1xvXEdwEuC0w";
+  //   var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zipcode + "&key=" + key;;
+  //   console.log(queryURL);
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .done(function(response) {
-        console.log(response);
-        var results = response.results[0].address_components;
-        var city = results[1].long_name;
-        var stateAbv = results[4].short_name;
-        $('#zip-input').val("");
-        location.href = "budget.html";
-      })
+  //   $.ajax({
+  //       url: queryURL,
+  //       method: "GET"
+  //     })
+  //     .done(function(response) {
+  //       console.log(response);
+  //       var results = response.results[0].address_components;
+  //       var city = results[1].long_name;
+  //       var stateAbv = results[4].short_name;
+  //       $('#zip-input').val("");
+  //       location.href = "budget.html";
+  //     })
 
 
-  });
+  // });
+
+
 
   //Average gas call
 
@@ -45,34 +47,37 @@ $(document).ready(function() {
   //NASDAQ call
   var queryURLNasdaq = "https://www.quandl.com/api/v3/datasets/NASDAQOMX/COMP.json?api_key=YLrr6a7wymzzjhMCd_xr&start_date=2017-08-12";
 
-  $.ajax({
-      url: queryURLNasdaq,
-      method: "GET"
-    })
-    .done(function(numbers) {
-      var numbers = numbers.dataset.data[0][1];
-      console.log(numbers);
-      $("#nasdaq").text(numbers)
-    })
+  // $.ajax({
+  //     url: queryURLNasdaq,
+  //     method: "GET"
+  //   })
+  //   .done(function(numbers) {
+  //     var numbers = numbers.dataset.data[0][1];
+  //     console.log(numbers);
+  //     $("#nasdaq").text(numbers)
+  //   })
 
-  //S&P call
-  var queryURLSP = "https://www.quandl.com/api/v3/datasets/YALE/SPCOMP.json?api_key=YLrr6a7wymzzjhMCd_xr";
-  $.ajax({
-      url: queryURLSP,
-      method: "GET"
-    })
-    .done(function(spnumbers) {
-      var spnumbers = spnumbers.dataset.data[0][1];
-      $("#sp").text(spnumbers);
-    })
+  // //S&P call
+  // var queryURLSP = "https://www.quandl.com/api/v3/datasets/YALE/SPCOMP.json?api_key=YLrr6a7wymzzjhMCd_xr";
+  // $.ajax({
+  //     url: queryURLSP,
+  //     method: "GET"
+  //   })
+  //   .done(function(spnumbers) {
+  //     var spnumbers = spnumbers.dataset.data[0][1];
+  //     $("#sp").text(spnumbers);
+  //   })
 
   //Makin' ID's
-  var idCount = 0;
+  var incomeCount = 1; // count starts here because there is already 1 income item
+  var expenseCount = 4; // count starts here because there are alread 4 expense items
+  var incomeArray = [];
+  var expensesArray = [];
 
-  function getNextIds() {
-    idCount++;
-    return [("label_" + idCount), ("amount_" + idCount)]
-  }
+  // function getNextIds() {
+  //   idCount++;
+  //   return [("label_" + idCount), ("amount_" + idCount)]
+  // }
 
   // Dynamic add to results.
   $('#housing-amount-input').keyup(mirrorInput);
@@ -107,7 +112,8 @@ $(document).ready(function() {
   // Adding income button function.
   $("#add-income").click(function() {
     // Create the div
-    // var outerDiv = $('<div class="row repeating-income-row">')
+    var outerDiv = $('<div class="row repeating-income-row">');
+    incomeCount++;
 
 
     // $("<div class='row repeating-income-row'> <div class='col-md-6'> <div class='input-group'> <input type='text' class='form-control' id='income-label-input' aria-describedby='basic-addon1' placeholder='Type your custom input label here.'> </div> <br> </div> <div class='col-md-6'> <div class='input-group'> <input type='text' class='form-control' id='income-amount-input' aria-describedby='basic-addon1' placeholder='1000'> </div> </div> </div>")
@@ -117,12 +123,23 @@ $(document).ready(function() {
     //Put input in Div
     //Append to income-block
     $("#income-block").append("<div class='row repeating-income-row'> <div class='col-md-6'> <div class='input-group'> <input type='text' class='form-control' id='income-label-input' aria-describedby='basic-addon1' placeholder='Type your custom input label here.'> </div> <br> </div> <div class='col-md-6'> <div class='input-group'> <input type='text' class='form-control' id='income-amount-input' aria-describedby='basic-addon1' placeholder='1000'> </div> </div> </div>");
+    $("#income-label-input").attr("id", "income-label-input income-label-" + incomeCount);
+    $("#income-amount-input").attr("id", "income-amount-input income-" + incomeCount);
   });
   // Adding expense button function.
   $("#add-expenses").click(function() {
     $("#expenses-block").append("<div class='row repeating-expenses-row'> <div class='col-md-6'> <div class='expenses-group'> <input type='text' class='form-control' id='expenses-label-input' aria-describedby='basic-addon1' placeholder='Type your custom input label here.'> </div> <br> </div> <div class='col-md-6'> <div class='expenses-group'> <input type='text' class='form-control' id='expenses-amount-input' aria-describedby='basic-addon1' placeholder='1000'> </div> </div> </div>");
-
+    $("#expenses-label-input").attr("id", "expenses-label-input expenses-label-" + expenseCount);
+    $("#expenses-amount-input").attr("id", "expenses-amount-input expenses-" + expenseCount);
 
   });
+
+  // MATH FUNCTION
+
+  // Capture the values of the income amounts
+  // Capture the values of the expense amounts
+  // Create a math function that will add the passed values and store them in a 'total' variable
+
+  // function addInputs (x,y,z)
 
 });
